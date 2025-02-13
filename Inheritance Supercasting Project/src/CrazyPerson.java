7734226fb22051c8137a577b61afe12632e844d0
jacqueline.magolske <jacqueline.magolske@MU-C1-13.mullen.pvt.k12.co.us>
2025-02-13 13:18:21 -0700

    initial commit

---
+++ Inheritance Supercasting Project/src/CrazyPerson.java
@@ -0,0 +1,17 @@
+
+public class CrazyPerson
+	{
+		
+		public static void getObsessed()
+			{
+			System.out.println("I LOVE LOVE LOVE LOVE LOVE AHHH");
+			}
+			
+		public static void stalk()
+			{
+			System.out.println("I know everything about them");
+			}
+
+
+		public abstract void move();
+	}
---
+++ Inheritance Supercasting Project/src/TwilightFan.java
@@ -0,0 +1,15 @@
+
+public class TwilightFan
+	{
+
+		public static void jumpAcrossTrees()
+			{
+			System.out.println("Hold on tight spider monkey");
+			}
+			
+		public static void stalk()
+			{
+			System.out.println("Did you know Robert Pattinson is getting married to Suki Waterhouse instead of me????"); 
+			}
+		
+	}
